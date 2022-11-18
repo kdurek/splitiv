3e2b58ab88a8ea484c20c0b18786f67badfab79d
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2022-11-18 14:07:49 +0100

    fix: logo descendant error

--- splitiv-frontend/src/components/Layout/Logo.jsx
+++ splitiv-frontend/src/components/Layout/Logo.jsx
@@ -1,10 +1,18 @@
-import { Heading } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <Link to="/">
-      <Heading>Splitiv</Heading>
+    <Link
+      as={RouterLink}
+      to="/"
+      fontSize="2xl"
+      fontWeight={600}
+      _hover={{
+        textDecoration: "none",
+      }}
+    >
+      Splitiv
     </Link>
   );
 }

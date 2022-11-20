21f9e5e4ef57fe60770fa971d227ae6f6133e121
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2022-11-20 20:00:23 +0100

    refactor: add v1 api

--- splitiv-backend/router/router.go
+++ splitiv-backend/router/router.go
@@ -7,24 +7,24 @@ import (
 )
 
 func Initialize(router *fiber.App) {
-	groups := router.Group("/groups", middleware.TokenCheck)
+	groups := router.Group("/api/v1/groups", middleware.TokenCheck)
 	groups.Get("/", controller.GetGroups)
 	groups.Post("/", controller.CreateGroup)
 
-	group := router.Group("/groups/:groupId", middleware.TokenCheck)
+	group := router.Group("/api/v1/groups/:groupId", middleware.TokenCheck)
 	group.Get("/", controller.GetGroup)
 	group.Delete("/", controller.DeleteGroup)
 
-	groupUsers := router.Group("/groups/:groupId/users", middleware.TokenCheck)
+	groupUsers := router.Group("/api/v1/groups/:groupId/users", middleware.TokenCheck)
 	groupUsers.Post("/", controller.AddUserToGroup)
 	groupUsers.Delete("/", controller.DeleteUserFromGroup)
 
-	groupExpenses := router.Group("/groups/:groupId/expenses", middleware.TokenCheck)
+	groupExpenses := router.Group("/api/v1/groups/:groupId/expenses", middleware.TokenCheck)
 	groupExpenses.Get("/", controller.GetExpensesByGroup)
 	groupExpenses.Post("/", controller.CreateExpense)
 	groupExpenses.Delete("/:expenseId", controller.DeleteExpense)
 
-	users := router.Group("/users", middleware.TokenCheck)
+	users := router.Group("/api/v1/users", middleware.TokenCheck)
 	users.Get("/", controller.GetUsers)
 	users.Get("/me", controller.GetCurrentUser)
 }

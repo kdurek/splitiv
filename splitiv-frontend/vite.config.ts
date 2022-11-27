2f28cea037af1e9517e607ce2ebb2298da879314
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2022-11-27 15:00:39 +0100

    chore: add vitest and testing library

--- splitiv-frontend/vite.config.ts
+++ splitiv-frontend/vite.config.ts
@@ -1,8 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import Pages from "vite-plugin-pages";
+import pages from "vite-plugin-pages";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Pages()],
+  plugins: [react(), pages()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    // Remove when fixed
+    deps: {
+      fallbackCJS: true,
+    },
+  },
 });

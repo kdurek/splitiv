e50259951aec9c8e996bac1abb9e9eadbdb78492
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2022-12-18 12:58:14 +0100

    fix: wrong debt values

--- splitiv-fastify/src/utils/generateDebts.ts
+++ splitiv-fastify/src/utils/generateDebts.ts
@@ -1,19 +1,3 @@
-/* eslint-disable no-continue */
-/* eslint-disable no-restricted-syntax */
-/* eslint-disable no-param-reassign */
-
-import {
-  add,
-  isNegative,
-  isPositive,
-  minimum,
-  multiply,
-  subtract,
-  toUnit,
-} from "dinero.js";
-
-import { generateBalances } from "./generateBalances";
-
 import type { IExpenseUser } from "./generateBalances";
 
 interface IDebt {
@@ -22,52 +6,24 @@ interface IDebt {
   amount: string;
 }
 
-function overwrite<T>(array: T[], element: T, elementProp: keyof T) {
-  const i = array.findIndex(
-    (_element) => _element[elementProp] === element[elementProp]
-  );
-  if (i > -1) {
-    array[i] = element;
-  }
-}
-
 export function generateDebts(users: IExpenseUser[]) {
-  const usersBalanceArray = generateBalances(users);
+  const fromUsers = users.filter((user) => parseFloat(user.paid) > 0);
+  const toUsers = users.filter((user) => parseFloat(user.owed) > 0);
 
   const debts: IDebt[] = [];
 
-  usersBalanceArray.forEach((from) => {
-    const { userId: fromId, amount: fromAmount } = from;
-    if (isPositive(fromAmount)) {
-      usersBalanceArray.forEach((to) => {
-        const { userId: toId, amount: toAmount } = to;
-        if (toId !== fromId && isNegative(toAmount)) {
-          const fromAmountTrue = usersBalanceArray.find(
-            (user) => user.userId === fromId
-          )!.amount;
-          const toAmountPositive = multiply(toAmount, -1);
-          const amount = minimum([fromAmountTrue, toAmountPositive]);
-
-          overwrite(
-            usersBalanceArray,
-            { userId: fromId, amount: subtract(fromAmountTrue, amount) },
-            "userId"
-          );
-
-          overwrite(
-            usersBalanceArray,
-            { userId: toId, amount: add(toAmount, amount) },
-            "userId"
-          );
-
-          debts.push({
-            fromId: toId,
-            toId: fromId,
-            amount: toUnit(amount).toFixed(2),
-          });
-        }
-      });
-    }
+  fromUsers.forEach((from) => {
+    const { userId: fromId, paid: fromAmount } = from;
+    toUsers.forEach((to) => {
+      const { userId: toId, owed: toAmount } = to;
+      if (fromAmount === toAmount) {
+        debts.push({
+          fromId: toId,
+          toId: fromId,
+          amount: fromAmount,
+        });
+      }
+    });
   });
 
   return debts;

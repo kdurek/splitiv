b26c1aa8f29686ef4f6bb27d05cc7d9eba18976f
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2022-12-18 18:58:27 +0100

    fix: cumulate debts instead of overwriting

--- splitiv-fastify/src/utils/generateDebts.ts
+++ splitiv-fastify/src/utils/generateDebts.ts
@@ -6,6 +6,26 @@ interface IDebt {
   amount: string;
 }
 
+function upsertDebt<T extends { amount: string }>(
+  array: T[],
+  element: T,
+  elementProp: keyof T,
+  secondElementProp: keyof T
+) {
+  const i = array.findIndex(
+    (_element) =>
+      _element[elementProp] === element[elementProp] &&
+      _element[secondElementProp] === element[secondElementProp]
+  );
+
+  if (i > -1) {
+    // eslint-disable-next-line no-param-reassign
+    array[i]!.amount = (
+      parseFloat(array[i]!.amount) + parseFloat(element.amount)
+    ).toFixed(2);
+  } else array.push(element);
+}
+
 export function generateDebts(users: IExpenseUser[]) {
   const fromUsers = users.filter((user) => parseFloat(user.paid) > 0);
   const toUsers = users.filter((user) => parseFloat(user.owed) > 0);
@@ -17,11 +37,16 @@ export function generateDebts(users: IExpenseUser[]) {
     toUsers.forEach((to) => {
       const { userId: toId, owed: toAmount } = to;
       if (fromAmount === toAmount) {
-        debts.push({
-          fromId: toId,
-          toId: fromId,
-          amount: fromAmount,
-        });
+        upsertDebt(
+          debts,
+          {
+            fromId: toId,
+            toId: fromId,
+            amount: fromAmount,
+          },
+          "fromId",
+          "toId"
+        );
       }
     });
   });

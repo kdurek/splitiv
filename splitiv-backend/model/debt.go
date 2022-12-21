0fb745b9ad01bbe9c72cff3b2a1aa46719d8efc9
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2022-12-21 01:56:58 +0100

    chore: remove old backend

--- splitiv-backend/model/debt.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Debt struct {
-	ExpenseID uint   `json:"expenseId,omitempty"`
-	FromID    uint   `json:"from,omitempty"`
-	ToID      uint   `json:"to,omitempty"`
-	Amount    string `json:"amount,omitempty"`
-}

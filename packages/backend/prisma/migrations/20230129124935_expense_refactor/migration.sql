2c932f7eeb4714841750ee83e5967d40bba4a0f9
Krzysztof Durek <krzysztof_durek@discovery.com>
2023-01-29 13:50:01 +0100

    refactor: change name to noun

--- packages/backend/prisma/migrations/20230129124935_expense_refactor/migration.sql
+++ packages/backend/prisma/migrations/20230129124935_expense_refactor/migration.sql
@@ -3,6 +3,7 @@
 
   - You are about to drop the column `type` on the `Expense` table. All the data in the column will be lost.
   - You are about to drop the `ExpenseUsers` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `UserGroups` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `payerId` to the `Expense` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -12,12 +13,29 @@ ALTER TABLE "ExpenseUsers" DROP CONSTRAINT "ExpenseUsers_expenseId_fkey";
 -- DropForeignKey
 ALTER TABLE "ExpenseUsers" DROP CONSTRAINT "ExpenseUsers_userId_fkey";
 
+-- DropForeignKey
+ALTER TABLE "UserGroups" DROP CONSTRAINT "UserGroups_groupId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserGroups" DROP CONSTRAINT "UserGroups_userId_fkey";
+
 -- AlterTable
 ALTER TABLE "Expense" DROP COLUMN "type",
 ADD COLUMN     "payerId" TEXT NOT NULL;
 
 -- DropTable
 DROP TABLE "ExpenseUsers";
+
+-- DropTable
+DROP TABLE "UserGroups";
+
+-- CreateTable
+CREATE TABLE "UserGroup" (
+    "userId" TEXT NOT NULL,
+    "groupId" TEXT NOT NULL,
+
+    CONSTRAINT "UserGroup_pkey" PRIMARY KEY ("userId","groupId")
+);
 
 -- CreateTable
 CREATE TABLE "ExpenseDebt" (
@@ -31,6 +49,12 @@ CREATE TABLE "ExpenseDebt" (
 
     CONSTRAINT "ExpenseDebt_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "UserGroup" ADD CONSTRAINT "UserGroup_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserGroup" ADD CONSTRAINT "UserGroup_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "ExpenseDebt" ADD CONSTRAINT "ExpenseDebt_expenseId_fkey" FOREIGN KEY ("expenseId") REFERENCES "Expense"("id") ON DELETE CASCADE ON UPDATE CASCADE;

87dafff40f38ecaa477f61ace07d76392d10ef3f
Krzysztof Durek <krzysztof_durek@discovery.com>
2023-02-13 17:59:53 +0100

    build: ignore type check

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.

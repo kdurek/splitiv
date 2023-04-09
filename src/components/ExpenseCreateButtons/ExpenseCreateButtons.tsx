b55e03954e7160d3e12280273a1909963aad3858
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2023-04-09 23:38:58 +0200

    feat: new create expense form

--- src/components/ExpenseCreateButtons/ExpenseCreateButtons.tsx
+++ src/components/ExpenseCreateButtons/ExpenseCreateButtons.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "@mantine/core";
 
 import CreateExpenseModal from "components/ExpenseForm/CreateExpenseModal";
+import ExpenseFormRevamped from "components/ExpenseFormRevamped/ExpenseFormRevamped";
 import ExpensePayListModal from "components/ExpensePayList/ExpensePayListModal";
 
 function ExpenseCreateButtons() {
   return (
     <Stack>
+      <ExpenseFormRevamped />
       <CreateExpenseModal />
       <ExpensePayListModal />
     </Stack>
---
+++ src/components/ExpenseFormRevamped/ExpenseFormSchemaRevamped.ts
@@ -0,0 +1,41 @@
+import Decimal from "decimal.js";
+import { z } from "zod";
+
+export const ExpenseFormSchemaRevamped = z
+  .object({
+    name: z.string().min(3, { message: "Minimalna długość to 3 znaki" }),
+    description: z
+      .union([
+        z.string().min(3, { message: "Minimalna długość to 3 znaki" }),
+        z.string().length(0),
+      ])
+      .optional(),
+    amount: z.number().gt(0, { message: "Kwota musi być większa niż zero" }),
+    payer: z.string().cuid2("Musisz wybrać osobę płacącą"),
+    debts: z.array(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        amount: z.number(),
+      })
+    ),
+  })
+  .refine(
+    (values) => {
+      const usedAmount = Number(
+        values.debts.reduce(
+          (prev, curr) => Decimal.add(prev, curr.amount),
+          new Decimal(0)
+        )
+      );
+      return values.amount === usedAmount;
+    },
+    {
+      message: "Kwota wydatku nie jest równo rozdzielona pomiędzy użytkowników",
+      path: ["debts"],
+    }
+  );
+
+export type ExpenseFormValuesRevamped = z.infer<
+  typeof ExpenseFormSchemaRevamped
+>;

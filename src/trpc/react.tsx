b0bdf1caa55653db1fb6ed515276e5f4db1d09d9
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-02-07 00:03:02 +0100

    chore: cleanup

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { httpBatchLink, loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
+import { httpBatchLink, loggerLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { useState } from 'react';
 

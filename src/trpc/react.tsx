b633995f3913d3ecc2bfc9b5b2a6e2fe31721809
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-04-10 23:38:15 +0200

    refactor: clean up code

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -4,9 +4,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { useState } from 'react';
+import SuperJSON from 'superjson';
 
 import { type AppRouter } from '@/server/api/root';
-import { getUrl, transformer } from '@/trpc/shared';
 
 const createQueryClient = () => new QueryClient();
 
@@ -49,8 +49,8 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             process.env.NODE_ENV === 'development' || (op.direction === 'down' && op.result instanceof Error),
         }),
         unstable_httpBatchStreamLink({
-          transformer,
-          url: getUrl(),
+          transformer: SuperJSON,
+          url: getBaseUrl() + '/api/trpc',
           headers: () => {
             const headers = new Headers();
             headers.set('x-trpc-source', 'nextjs-react');
@@ -68,4 +68,10 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
       </api.Provider>
     </QueryClientProvider>
   );
+}
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') return window.location.origin;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 }

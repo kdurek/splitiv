d3e0dda4aa6cc994f6518e6f11075efeec7907ed
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-05-07 23:11:56 +0200

    chore: update packages

--- next.config.js
+++ next.config.js
@@ -4,12 +4,19 @@
  */
 await import('./src/env.js');
 
+// @ts-check
 import withSerwistInit from '@serwist/next';
+
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+const revision = crypto.randomUUID();
 
 const withSerwist = withSerwistInit({
   cacheOnNavigation: true,
   swSrc: 'src/app/sw.ts',
   swDest: 'public/sw.js',
+  additionalPrecacheEntries: [{ url: '/~offline', revision }],
 });
 
 /** @type {import("next").NextConfig} */
--- src/app/sw.ts
+++ src/app/sw.ts
@@ -1,9 +1,6 @@
-import type { SerwistGlobalConfig } from '@serwist/core';
-import { ExpirationPlugin } from '@serwist/expiration';
 import { defaultCache } from '@serwist/next/worker';
-import type { PrecacheEntry } from '@serwist/precaching';
-import { NetworkOnly } from '@serwist/strategies';
-import { Serwist } from '@serwist/sw';
+import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist';
+import { ExpirationPlugin, NetworkOnly, Serwist } from 'serwist';
 
 import { notificationEventSchema, pushEventSchema } from '@/lib/validations/push';
 
@@ -11,15 +8,44 @@ declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
     // Change this attribute's name to your `injectionPoint`.
     // `injectionPoint` is an InjectManifest option.
-    // See https://serwist.pages.dev/docs/build/inject-manifest/configuring
+    // See https://serwist.pages.dev/docs/build/configuring
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-  }
-  interface Window {
-    serwist: Serwist;
   }
 }
 
 declare const self: ServiceWorkerGlobalScope;
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: [
+    {
+      matcher: /\/api\/auth\/.*/,
+      handler: new NetworkOnly({
+        plugins: [
+          new ExpirationPlugin({
+            maxEntries: 16,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          }),
+        ],
+        networkTimeoutSeconds: 10, // fallback to cache if API does not response within 10 seconds
+      }),
+    },
+    ...defaultCache,
+  ],
+  fallbacks: {
+    entries: [
+      {
+        url: '/~offline',
+        matcher({ request }) {
+          return request.destination === 'document';
+        },
+      },
+    ],
+  },
+});
 
 self.addEventListener('push', (event) => {
   const pushEvent = pushEventSchema.parse(event.data?.json());
@@ -44,39 +70,4 @@ self.addEventListener('notificationclick', (event) => {
   event.waitUntil(self.clients.openWindow(notificationEvent.data.url));
 });
 
-const serwist = new Serwist();
-// Anything random.
-const revision = crypto.randomUUID();
-
-serwist.install({
-  precacheEntries: self.__SW_MANIFEST,
-  skipWaiting: true,
-  clientsClaim: true,
-  navigationPreload: true,
-  runtimeCaching: [
-    {
-      matcher: /\/api\/auth\/.*/,
-      handler: new NetworkOnly({
-        plugins: [
-          new ExpirationPlugin({
-            maxEntries: 16,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          }),
-        ],
-        networkTimeoutSeconds: 10, // fallback to cache if API does not response within 10 seconds
-      }),
-    },
-    ...defaultCache,
-  ],
-  fallbacks: {
-    entries: [
-      {
-        url: '/~offline',
-        revision,
-        matcher({ request }) {
-          return request.destination === 'document';
-        },
-      },
-    ],
-  },
-});
+serwist.addEventListeners();

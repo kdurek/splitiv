78895f3eb9c11f0ad82e45c607beb749b7f1992e
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-07-29 15:48:08 +0300

    feat: fix loading in search

--- src/components/expense/expenses-search.tsx
+++ src/components/expense/expenses-search.tsx
@@ -1,11 +1,16 @@
+import { Suspense } from 'react';
+
 import { ExpenseSearchInput } from '@/components/expense/expenses-search-input';
 import { ExpensesSearchList } from '@/components/expense/expenses-search-list';
+import { FullScreenLoading } from '@/components/layout/loading';
 
 export default function ExpensesSearch() {
   return (
     <div className="space-y-4">
       <ExpenseSearchInput />
-      <ExpensesSearchList />
+      <Suspense fallback={<FullScreenLoading />}>
+        <ExpensesSearchList />
+      </Suspense>
     </div>
   );
 }

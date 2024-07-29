8c63e8123e528774cda5a17c45f4fb2a97bbc79f
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-07-29 17:30:58 +0300

    refactor: update loading component to use primary color for spinner

--- src/components/layout/loading.tsx
+++ src/components/layout/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2Icon } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 export function Loader({ className }: { className?: string }) {
-  return <Loader2Icon className={cn('size-16 animate-spin text-muted-foreground', className)} />;
+  return <Loader2Icon className={cn('size-16 animate-spin text-primary/50', className)} />;
 }
 
 export function FullScreenLoading({ className }: { className?: string }) {

693bbce1a1b331e0b8ea69eb004c422f43bc615d
Krzysztof Durek <21038648+kdurek@users.noreply.github.com>
2024-10-21 12:01:45 +0200

    chore: update packages

--- src/components/auth/sign-out-button.tsx
+++ src/components/auth/sign-out-button.tsx
@@ -2,7 +2,7 @@
 
 import { LogOut } from 'lucide-react';
 import { useTranslations } from 'next-intl';
-import { useFormStatus } from 'react-dom';
+import { useFormState, useFormStatus } from 'react-dom';
 
 import { Loader } from '@/components/layout/loading';
 import { Button } from '@/components/ui/button';
@@ -10,10 +10,13 @@ import { signOutAction } from '@/lib/actions/auth';
 
 export function SignOutButton() {
   const t = useTranslations('LogoutButton');
+  const [_, formAction] = useFormState(signOutAction, {
+    error: '',
+  });
   const { pending } = useFormStatus();
 
   return (
-    <form action={signOutAction}>
+    <form action={formAction}>
       <Button type="submit" variant="destructive" className="w-full" disabled={pending}>
         {pending && <Loader className="mr-2 size-4 animate-spin" />}
         <LogOut className="mr-2" /> {t('text')}
